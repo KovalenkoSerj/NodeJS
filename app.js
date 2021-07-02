8f5932a6d4fc0b3abe62b676b17f6fdecf12e138
Сергей Коваленко <spekt.92@gmail.com>
2021-07-02 15:38:49 +0300

    test-task done

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
-const routes = require('./routes')
 const express = require('express');
-
 const app = express();
+const PORT = 3000
+
+
+
+// app.use((req, res, next) => {
+//     console.log("i'm in the First page") // always running
+//     next()
+// })
+
+// app.use((req, res, next) => {
+//     res.send('<h1>Second page</h1>')
+//     console.log("i'm in the second page")
+// })
+
+
+
+app.use('/users', (req, res, next) => {
+    res.send('<h1>Result page</h1>')
+    console.log("i'm in the result page")
+})
 
 app.use('/', (req, res, next) => {
-    console.log('In the product middleware');
-    res.send('<h1>Hello from express</h1>')
+    res.send('<h1>Initial page</h1>')
+    console.log("i'm in the initial page")
 })
 
 
-app.use('/product', (req, res, next) => {
-    console.log('In the next middleware');
-    res.send('<h1>Hello from product</h1>')
-})
-
-
-
-
-
-app.listen(3000)
+app.listen(PORT)
